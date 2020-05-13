60c9dfdd6d552a11d63b4bea3ec18d7423fe15d2
aaaa0976313036 <34019325+aaaa0976313036@users.noreply.github.com>
2020-05-13 11:39:25 +0800

    Add files via upload

---
+++ 04050263-HW07.ino
@@ -0,0 +1,19 @@
+#include <LiquidCrystal.h>
+
+
+LiquidCrystal lcd(12,11,5,4,3,2);
+
+void setup() {
+  Serial.begin(9600);
+  lcd.begin(16,2);
+  lcd.clear();
+}
+
+void loop() {
+
+ lcd.setCursor(0,0);
+ lcd.print("04050263 pico");
+ 
+ lcd.setCursor(0,1);
+
+}
